a478d02cbbbbad8a1393eb11dd36bab644335718
murilo-cassiani <murilo.cassiani@gmail.com>
2021-03-25 17:34:41 -0300

    sintax database usada no spring02

---
+++ aula09/sintax_usadas.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE aula09;
+
+USE aula09;
+
+INSERT INTO usuario
+  VALUES(null, "1234567", "teste@exemplo.com", "Teste da Silva", "1234");
+  
+INSERT INTO usuario
+  VALUES(null, "77744422288", "maria@exemplo.com", "Maria da Silva", "4321");
